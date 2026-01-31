bace717e159a718977040af25e345652a79744de
harshtelangework-star <harshtelangework@gmail.com>
2026-01-31 21:22:21 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+//Simple Calculator
+//Sum
+//Difference
+//Product
+//Quotient
+
+public class main{
+    public static void main(String[]args){
+        
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
